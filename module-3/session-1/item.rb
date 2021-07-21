f9315b8079bab2feccf812edb69c99d6b55cfaa0
timothybagas <63138796+timothybagas@users.noreply.github.com>
2021-07-21 12:23:43 +0700

    menambahkan item.rb

---
+++ module-3/session-1/item.rb
@@ -0,0 +1,12 @@
+require_relative 'category'
+
+class Item
+	attr_reader :id, :name, :price, :category
+
+	def initialize(id, name, price, category=nil)
+		@id = id
+		@name = name
+		@price = price
+		@category = category
+	end
+end
