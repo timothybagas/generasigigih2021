f49918d2175fb6a6d0c3bc28553912ca2c65458c
timothybagas <timothyhaullussy@gmail.com>
2021-08-04 12:39:39 +0700

    fixing typo USSERNAME to USERNAME

--- module-3/session-3/db/db_connector.rb
+++ module-3/session-3/db/db_connector.rb
@@ -3,7 +3,7 @@ require 'mysql2'
 def create_db_client
 	Mysql2::Client.new(
 		:host => "localhost",
-		:username => ENV["DB_USSERNAME"],
+		:username => ENV["DB_USERNAME"],
 		:password => ENV["DB_PASSWORD"],
 		:database => ENV["DB_NAME"])
 end
